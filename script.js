bd19d4b664b3babbc72f3b75184ec2f20bfece72
OrfilaBohnetJeronimo <136343290+OrfilaBohnetJeronimo@users.noreply.github.com>
2023-08-07 10:04:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+num1 = 6
+console.log('La suma, resta, divicion y multiplicacion de 6 y 5')
+
+console.log('La suma es', suma = num1 +5)
+console.log('La resta es', resta = num1 -5)
+console.log('la division es', divicion = num1 /5)
+console.log('La multiplicacion es', multiplicacion = num1 *5)
+
+num2 = 4
+console.log('La suma, resta, divicion y multiplicacion de 4 y 7')
+
+console.log('La suma es', suma = num2 +7)
+console.log('La resta es', resta = num2 -7)
+console.log('la division es', divicion = num2 /7)
+console.log('La multiplicacion es', multiplicacion = num2 *7)
+
+
+alert('La suma, resta, divicion y multiplicacion de 3 y 2')
+
+alert('La suma es ' + (3 + 2))
+alert('La resta es ' + (3 - 2))
+alert('La division es ' + (3 / 2))
+alert('La multiplicacion es ' + (3 * 2))
+
+
+alert('La suma, resta, divicion y multiplicacion de 8 y 9')
+
+alert('La suma es ' + (8 + 9))
+alert('La resta es ' + (8 - 9))
+alert('La division es ' + (8 / 9))
+alert('La multiplicacion es ' + (8 * 9))
+
+//prompt('¿En que año naciste?');
+var añoDeNacimiento = prompt('¿En que año naciste?');
+//prompt('¿Que año es?');
+var añoActual = prompt('¿Que año es?');
+alert('Tenes ' + (añoActual - añoDeNacimiento) + ' años')
+
+var nombre = prompt('¿Cual es tu nombre?');
+var apellido = prompt('¿Cual es tu apellido?');
+alert('Un gusto conocerte ' + nombre + ' ' + apellido)
+console.log('Un gusto conocerte ' + nombre + ' ' + apellido)
+
+var diez = 10
+var tres = 3
+var resultadoMultiplicacion = 10 * 3
+alert('El resultado de 10 por 3 es ' + resultadoMultiplicacion)
+console.log('El resultado de 10 por 3 es ' + resultadoMultiplicacion)
